05e7cd91a739533fe62855f1f4acb6479bb34a48
unknown <abijithmv01@gmail.com>
2023-09-26 23:05:32 +0530

    added chnages

--- revision/revision.swift
+++ revision/revision.swift
@@ -9,8 +9,10 @@ let person1 = Person(name: "Abj")
 let person2 = Person(name: "Abj")
 let person3 = person1
 
+// Equality operator - values
 // print(person1 == person2)
 // print(person1 === person2)
+// Identity operator - memory references
 print(person1 === person3)
 
 // Binary search
@@ -21,6 +23,7 @@ func BinarySearch(value: Int, arr: [Int]){
     var middle: Int
     while(left<=right){
         middle = (left+right)/2
+        print("middle index \(middle), Left index \(left), Right index \(right)")
         if(arr[middle] == value){
             print("Element found at position \(middle+1)")
             return
@@ -36,20 +39,77 @@ func BinarySearch(value: Int, arr: [Int]){
 }
 
 
-BinarySearch(value: 5, arr: [1,2,3,4])
+BinarySearch(value: 2, arr: [1,2,3,4,5])
 
 //swap values
 
-func swapInt(_ a: inout Int, _ b: inout Int){
-    (a , b) = (b, a)
-}
+// Tuple Deconstruction
 
 var val1 = 1
 var val2 = 2
 print("Before swapping: val1 = \(val1) val2 = \(val2)")
-swapInt(&val1, &val2)
+(val1, val2) = (val2, val1)
 print("After swapping: val1 = \(val1) val2 = \(val2)")
 
+// xor
+
+var a = 10
+var b = 5
+
+a = a ^ b
+b = a ^ b
+a = a ^ b
+
+print("The values: a = \(a) and b  = \(b)")
+
+a = 10
+b = 5
+
+a = a+b
+b = a-b
+a = a-b
+
+print("The values: a = \(a) and b  = \(b)")
+
+
+// Circular reference
+
+class Student {
+    let name: String
+    weak var apartment: Classes?
+
+    init(name: String) {
+        self.name = name
+        print("\(name) is being initialized.")
+    }
+
+    deinit {
+        print("\(name) is being deinitialized.")
+    }
+}
+
+class Classes {
+    let unit: String
+    weak var occupant: Student?
+
+    init(unit: String) {
+        self.unit = unit
+        print("Apartment \(unit) is being initialized.")
+    }
+
+    deinit {
+        print("Apartment \(unit) is being deinitialized.")
+    }
+}
+
+var std: Student? = Student(name: "Abj")
+var cls: Classes? = Classes(unit: "1bhk")
+
+std?.apartment = cls
+cls?.occupant = std
+
+std = nil
+cls = nil
 
 //Permutations
 //n!/(n-r)!
@@ -65,12 +125,10 @@ func permutations(_ n: Int, _ r: Int) -> Int {
     return factorial(n) / factorial(n - r)
 }
 
-
-let pn = 5
-let pr = 3
+let pn = 3
+let pr = 5
 let nPr = permutations(pn, pr)
 print("\(pn)P\(pr) = \(nPr)")
-
 
 //Combinations
 //n!/r!*(n-r)!
@@ -79,8 +137,7 @@ func combinations(_ n: Int, _ r: Int) -> Int {
     return factorial(n) / (factorial(r) * factorial(n - r))
 }
 
-
-let cn = 5
-let cr = 3
+let cn = 3
+let cr = 5
 let nCr = combinations(cn, cr)
 print("\(cn)C\(cr) = \(nCr)")
